9f0c256e658ad9bf7c7747fba3b1ba5dfb0ef2c9
Robin Zevallos <robin.zevallos@gestionysistemas.com>
2020-12-13 13:20:26 -0500

    API folder

---
+++ Haka.Skia.Test/BaseTest.cs
@@ -0,0 +1,22 @@
+﻿using Haka.Core.Test;
+using System;
+using Xunit;
+
+namespace Haka.Skia.Test
+{
+    public class BaseTest : IClassFixture<MainFixture>
+    {
+    }
+
+    public class MainFixture : IDisposable
+    {
+        public MainFixture()
+        {
+            XamarinMock.Init(this);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
